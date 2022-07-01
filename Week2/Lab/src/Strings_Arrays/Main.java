7eeed3b29e0fb90d33fc578828ab76ce6fb797be
Devadavid <s3927112@rmit.edu.vn>
2022-07-01 19:46:09 +0700

    new push

--- Week2/Lab/src/Strings_Arrays/Main.java
+++ Week2/Lab/src/Strings_Arrays/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str;
-        Month_Year_Days ex1 = new Month_Year_Days();
-        ex1.monthYearsDays();
+       /* Month_Year_Days ex1 = new Month_Year_Days();
+        ex1.monthYearsDays();*/
 
-        Cities_Sorting ex2 = new Cities_Sorting();
-        ex2.citiesSorting();
+       /* Cities_Sorting ex2 = new Cities_Sorting();
+        ex2.citiesSorting();*/
 
-        Reserve_String ex3 = new Reserve_String();
+        /*Reserve_String ex3 = new Reserve_String();
         System.out.print("Enter a String: ");
         str = scanner.nextLine();
         System.out.println("Reserve String of " + str +  " is: " + ex3.reserveString(str));
@@ -22,8 +22,23 @@ public class Main {
             System.out.println(str + " is Palindrome");
         } else {
             System.out.println(str + " isn't Palindrome");
-        }
+        }*/
 
+        Sorted_List ex5 = new Sorted_List();
+        while(true) {
+            System.out.print("Enter list: ");
+            str = scanner.nextLine();
+            if(str.charAt(0) == '0') {
+                System.out.println("Program exits");
+                break;
+            }
+            if(ex5.isSortedList(str)) {
+                System.out.println("The list is already sorted");
+            } else {
+                System.out.println("The list is not sorted");
+            }
+
+        }
 
     }
 }
---
+++ Week2/Lab/src/Strings_Arrays/Sorted_List.java
@@ -0,0 +1,28 @@
+package Strings_Arrays;
+
+import java.util.Arrays;
+
+/*Write a program that determines if a list is already sorted in increasing order. The
+program should prompt the user to enter a list and displays whether the list is sorted in
+ascending order or not. Note that the first number in the input indicates the number of
+the elements in the list. The program exits when the first number is 0.*/
+public class Sorted_List {
+    public static boolean isSortedList(String str) {
+        // Convert str to String Arr
+        String[] strArray = null;
+        strArray = str.split(" ");
+
+        // Convert String Arr to Int Arr
+        int[] list = new int[Integer.parseInt(strArray[0])];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = Integer.parseInt(strArray[i + 1]);
+        }
+
+        for(int i = 0; i < list.length - 1; i++) {
+            if(list[i] > list[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
