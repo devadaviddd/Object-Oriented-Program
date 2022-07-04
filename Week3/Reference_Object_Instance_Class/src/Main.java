1597496fa35d2555fc29836d42dd11230ee475fd
Devadavid <s3927112@rmit.edu.vn>
2022-07-04 16:15:06 +0700

    Reference vs Object vs Instance vs Class

---
+++ Week3/Reference_Object_Instance_Class/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        House blueHouse = new House("blue");
+        House anotherHouse = blueHouse;
+
+        System.out.println(blueHouse.getColor()); // prints blue
+        System.out.println(anotherHouse.getColor()); // blue
+
+        anotherHouse.setColor("red");
+        System.out.println(blueHouse.getColor()); // red
+        System.out.println(anotherHouse.getColor()); // red because anotherHouse has reference of blueHouse
+
+        House greenHouse = new House("green");
+        anotherHouse = greenHouse;
+
+        System.out.println(blueHouse.getColor()); // red
+        System.out.println(greenHouse.getColor()); // green
+        System.out.println(anotherHouse.getColor()); // green because anotherHouse now has reference of greenHouse
+
+
+    }
+}
