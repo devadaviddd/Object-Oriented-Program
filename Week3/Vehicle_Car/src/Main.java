da5f7f306b32998e5257527c69986dafdbf2915a
Devadavid <s3927112@rmit.edu.vn>
2022-07-04 23:58:57 +0700

    velhicle and Car

---
+++ Week3/Vehicle_Car/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /* Challenge.
+        *  Start with a base class of Vehicle, then create a Car class that inherits from this base class.
+        *  Finally, create another class, a specific type of Car that inherits from the Car class.
+        *  You should be able to hand steering, changing gears, and moving (speed in other ways).
+        *  You will want to decide where to put the appropriate state and behaviours (fields and methods).
+        *  As mentioned above, changing gear, increasing/ decreasing speed should be included.
+        *  For you specific type of vehicle you will want to add something specific for that type of car*/
+    }
+}
