0dbeed2d96e356c37891611a98498f575fb6f738
Devadavid <s3927112@rmit.edu.vn>
2022-07-05 18:47:48 +0700

    Composition

--- Week3/Composition/src/Main.java
+++ Week3/Composition/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("220B", "Dell", "240", dimensions);
+
+        Monitor theMonitor = new Monitor("27inch Bease", "Acer", 27,
+                new Resolution(2540, 1440));
+
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4,
+                6, "v2.44");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+
+        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
     }
+
 }
