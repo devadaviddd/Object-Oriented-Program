5497639c22fb961d2c2b4b4faca14462a54ccb18
Devadavid <s3927112@rmit.edu.vn>
2022-07-05 20:45:39 +0700

    new

---
+++ Week3/Encapsulation/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        EnhancedPlayer player = new EnhancedPlayer("Tim", 50, "Sword");
+        System.out.println("Initial health is " + player.getHitPoints());
+
+    }
+}
